d19405984373a0de83c5f8981e1be529df8e5da9
Magno Martinez <66977118+Magno-12@users.noreply.github.com>
2022-10-12 18:25:12 -0500

    update settings.py

--- retochucknorris/settings.py
+++ retochucknorris/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://0.0.0.0:5575', 'https://djangoandfirebase-production.up.railway.app/']
+ALLOWED_HOSTS = ["*"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
